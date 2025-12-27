f0df231091d284bcf2a781c238be6c26c6ca8169
AshvekParab <24co10@aitdgoa.edu.in>
2025-12-27 21:05:12 +0530

    temp admin creation for render free plan

--- DriveData/backend/drivedata/settings.py
+++ DriveData/backend/drivedata/settings.py
@@ -182,3 +182,15 @@ SIMPLE_JWT = {
 
     'JTI_CLAIM': 'jti',
 }
+
+# TEMP ADMIN CREATION (REMOVE AFTER FIRST LOGIN)
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        username="admin",
+        email="admin@drivedata.com",
+        password="admin@123"
+    )
